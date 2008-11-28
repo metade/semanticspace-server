42e6b82b6c2c21086c28cc0599103fbdaab03563
Patrick Sinclair <metade@livro.gateway.2wire.net>
2008-11-28 14:26:27 +0000

    Initial commit

---
+++ camping_goodies.rb
@@ -0,0 +1,59 @@
+require 'action_view'
+
+PATH = File.dirname(__FILE__)
+
+module Camping::Controllers  
+  class Static < R '/static/(.+)'         
+    MIME_TYPES = {'.css' => 'text/css', '.js' => 'text/javascript', 
+                  '.jpg' => 'image/jpeg'}
+    PATH = File.expand_path(File.dirname(__FILE__))
+    
+    def get(path)
+      @headers['Content-Type'] = MIME_TYPES[path[/\.\w+$/, 0]] || "text/plain"
+      unless path.include? ".." # prevent directory traversal attacks
+        @headers['X-Sendfile'] = "#{PATH}/static/#{path}"
+      else
+        @status = "403"
+        "403 - Invalid path"
+      end
+    end
+  end
+end
+
+module CampingGoodies
+  include ActionView::Helpers
+  
+  def render(m, layout=true)
+    content = ERB.new(IO.read("#{PATH}/views/#{m}.html.erb")).result(binding)
+    content = ERB.new(IO.read("#{PATH}/views/layout.html.erb")).result(binding) if layout
+    return content
+  end  
+  
+  def not_found(type, brand)
+    content = layout { Mab.new{div.header! { h1(P);h2("#{type} #{brand} not found")} } }
+    r(404, content)
+  end
+  
+  def accept(format=nil)
+    if (format and format =~ /js(on)?/)
+      'application/json'
+    elsif (format and format =~ /ya?ml/)
+      'application/x-yaml'
+    elsif (format and format=~ /svg/)
+      'image/svg+xml'
+    else
+      env.ACCEPT.nil? ? (env.HTTP_ACCEPT.nil? ? 'text/html' : env.HTTP_ACCEPT) : env.ACCEPT
+    end
+  end
+  
+  def respond_to(format, object)
+    case accept(format)
+      when 'application/x-yaml' then
+        @headers['Content-Type'] = 'application/x-yaml'
+        object.to_yaml
+      when 'application/json' then
+        @headers['Content-Type'] = 'application/json'
+        object.to_json
+    end
+  end
+end
---
+++ semanticspace_server.rb
@@ -0,0 +1,116 @@
+require 'rubygems'
+require 'yaml'
+require 'json'
+require 'camping'
+require 'mime/types'
+
+require 'camping_goodies'
+require 'semanticspace'
+include SemanticSpace
+
+Camping.goes :SemanticspaceServer
+module SemanticspaceServer
+  include CampingGoodies
+  
+  if $space.nil?
+    config = YAML.load_file('config/semanticspace.yml')
+    puts "loading space: #{config[:space]}"
+    $space = SemanticSpace::read_semanticspace(config[:space])
+    $docs = YAML.load_file(config[:docs])
+    $terms = YAML.load_file(config[:terms])
+  end
+  
+  def space
+    $space
+  end
+end
+
+class Item
+  attr_accessor :name, :ident, :similarity
+  def initialize(type, struct)
+    self.ident = struct.ident
+    self.similarity = struct.similarity
+    self.name = case type
+      when :docs then doc_name(ident)
+      when :terms then term_name(ident)
+    end
+  end
+  
+  protected
+  
+  def doc_name(id)
+    $docs[id]
+  end
+  
+  def term_name(id)
+    $terms[id]
+  end
+end
+
+module SemanticspaceServer::Controllers
+  class Index < R '/'
+    def get
+      @title = 'Semantic Space Server'
+      render :index
+    end
+  end
+  
+  class About < R '/about'
+    def get
+      render :about
+    end
+  end
+  
+  class Docs < R '/docs', '/docs\.(.*)'
+    def get(format=nil)
+      @docs = space.list_docs(true)
+      respond_to(format, @docs) or render(:docs)
+    end
+  end
+  
+  class Doc < R '/docs/(.*?)', '/docs/(.*?)\.(.*)'
+    def get(doc, format=nil)
+      return not_found('doc', doc) unless space.list_docs(TRAINING_DOCUMENT_SPACE).include?(doc)
+      @docs = munge(:docs, space.search_with_doc(doc, true, TRAINING_DOCUMENT_SPACE, dimensions, 10))
+      @terms = munge(:terms, space.search_with_doc(doc, true, TERM_SPACE, dimensions, 10).map)
+      obj = { :docs => @docs, :terms => @terms }
+      respond_to(format, obj) or render(:doc)
+    end
+  end
+  
+  class Terms < R '/terms'
+    def get
+      @docs = space.list_terms(true)
+      respond_to(format, @docs) or render(:terms)
+    end
+  end
+  
+  class Term < R '/terms/(.*?)', '/terms/(.*?)\.(.*)'
+    def get(term, format=nil)
+      return not_found('term', term) unless space.list_terms.include?(term)
+      @docs = munge(:docs, space.search_with_term(term, TRAINING_DOCUMENT_SPACE, dimensions)[0,limit])
+      @terms = munge(:terms, space.search_with_term(term, TERM_SPACE, dimensions)[0,limit])
+      obj = { :docs => @docs, :terms => @terms }
+      respond_to(format, obj) or render(:term)
+    end
+  end
+  
+  protected
+  
+  def munge(type, array)
+    array.map { |i| Item.new(type, i) }
+  end
+  
+  def dimensions
+    44
+  end
+  
+  def limit
+    10
+  end
+end
+
+# this madness allows you to call R() from the ERB
+module CampingGoodies
+  include SemanticspaceServer::Controllers
+end
